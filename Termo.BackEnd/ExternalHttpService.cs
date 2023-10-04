985c0079d762509a757fab1287bf2596bcfe7cc0
diego.santana@sefaz.es.gov.br <diego.santana@sefaz.es.gov.br>
2023-10-04 16:28:42 -0300

    Método de validação das palavras criado com sucesso.

---
+++ Termo.BackEnd/ExternalHttpService.cs
@@ -0,0 +1,16 @@
+﻿namespace Termo.BackEnd {
+    public class ExternalHttpService {
+        private readonly HttpClient _httpClient;
+        public ExternalHttpService() {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<string>> GetWords() {
+            var response = await _httpClient.GetAsync("https://raw.githubusercontent.com/fserb/pt-br/master/palavras");
+            var concatedWords = await response.Content.ReadAsStringAsync();
+            var words = concatedWords.Split("\n").Where(p => p.Length == 5).ToList();
+
+            return words;
+        }
+    }
+}
--- Termo.BackEnd/Program.cs
+++ Termo.BackEnd/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Termo.BackEnd;
 
 var builder = WebApplication.CreateBuilder(args);
+var externanHttpService = new ExternalHttpService();
 
 builder.Services.AddDbContext<TermoContext>(context => context.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
@@ -17,12 +18,69 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () => {
+app.MapGet("/words", async (TermoContext context) => {
+    var day = DateTime.Now.Date;
+    var word = await context.DayWords.Where(w => w.Day == day).FirstOrDefaultAsync();
 
+    if (word == null) {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(word.Value);
+});
+
+app.MapGet("/words/validations", async (TermoContext context, string word) => {
+    if (word.Length != 5) {
+        return Results.BadRequest("Não atende ao padrão de 5 letras.");
+    }
+    var words = await externanHttpService.GetWords();
+    if (!words.Contains(word.ToLower())) {
+        return Results.BadRequest("Palavra não  aceita neste jogo.");
+    }
+
+    var day = DateTime.Now.Date;
+    var dayWord = await context.DayWords.Where(w => w.Day == day).FirstOrDefaultAsync();
+
+    if (dayWord == null) {
+        return Results.NotFound("Palavra não encontrada.");
+    }
+
+    return Results.Ok(ValidateWord(dayWord.Value, word));
+});
+
+//MÉTODO VERIFICA SE A LETRA EXISTE NA PALAVRA E SE A MESMA ESTÁ NA POSIÇÃO CERTA
+static WordResult ValidateWord(string dayWord, string wordAttempt) {
+    Letter[] letterResult = new Letter[dayWord.Length];
+
+    for (int i = 0; i < wordAttempt.Length; i++) {
+        var letterAttempt = wordAttempt[i];
+        bool exists, rightPlace;
+
+        exists = dayWord.Contains(letterAttempt);
+        rightPlace = dayWord[i] == letterAttempt;
+
+        letterResult[i] = new Letter(letterAttempt, exists, rightPlace);
+    }
+
+    return new WordResult(letterResult, dayWord == wordAttempt);
+}
+
+app.MapPost("/words", async (TermoContext context, string word) => {
+    var day = DateTime.Now.Date;
+
+    var wordDb = new DayWord() {
+        Day = day,
+        Value = word
+    };
+
+    await context.DayWords.AddAsync(wordDb);
+    await context.SaveChangesAsync();
 });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+/**
+ * ABAIXO, SEGUE UM NOVO JEITO DO .NET 6 CRIAR CLASSES. A PALAVRA RESERVADA 'RECORD', É O MEIO UTILIZADA PELO .NET PARA REPRESENTA UMA CLASSE.
+ */
+internal record Letter(char Value, bool Exists, bool RightPlace);
+internal record WordResult(Letter[] Letters, bool Success);
