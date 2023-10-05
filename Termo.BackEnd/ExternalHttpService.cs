c917a008a98cb6c16e8aab3b73b7f964d87586a8
diego.santana@sefaz.es.gov.br <diego.santana@sefaz.es.gov.br>
2023-10-05 13:49:48 -0300

    Adicionado a classe que faz a conexão com o repositório que possibilita a verificação das palavras.

--- Termo.BackEnd/ExternalHttpService.cs
+++ Termo.BackEnd/ExternalHttpService.cs
@@ -6,8 +6,13 @@
         }
 
         public async Task<List<string>> GetWords() {
+            //VAI AO REPOSITÓRIO
             var response = await _httpClient.GetAsync("https://raw.githubusercontent.com/fserb/pt-br/master/palavras");
+
+            //RECEBE O CONTEÚDO NA VARIÁVEL CONCATEDWORDS
             var concatedWords = await response.Content.ReadAsStringAsync();
+
+            //NORMALIZA A INFORMAÇÃO RECEBIDA
             var words = concatedWords.Split("\n").Where(p => p.Length == 5).ToList();
 
             return words;
