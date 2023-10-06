d935ccc5f128a7a3a40b83afb67713fb1d40ff2b
diego.santana@sefaz.es.gov.br <diego.santana@sefaz.es.gov.br>
2023-10-06 15:46:46 -0300

    Um monte no frontend.

--- Termo.FrontEnd/src/app/app.component.ts
+++ Termo.FrontEnd/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Termo.FrontEnd';
+  currentRow = 0;
+  success = false;
+
+  onClick(event: Event) {
+    if (!this.success) {
+      var div = event.target as HTMLElement;
+
+      if (div.parentElement?.getAttribute('row') == this.currentRow.toString()) {
+        div.parentElement?.querySelector(".edit")?.classList.remove('edit');
+        div?.classList.add('edit');
+      }
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  onKeyPress(event: KeyboardEvent) {
+    if (!this.success) {
+      var key = event.key
+
+      if (/[a-zA-Z]/.test(key) && key.length == 1) {
+        this.setCurrentPositionValue();
+        this.moveRight();
+      } else if (key == Key.ArrowRight) {
+        this.moveRight();
+      } else if (key == Key.ArrowLeft) {
+        this.moveLeft();
+      } else if (key == Key.Backspace) {
+        if (document.querySelector('.edit')?.innerHTML == '') {
+          this.moveLeft();
+        }
+        this.setCurrentPositionValue();
+      }
+    }
+  }
+
+  setCurrentPositionValue(value: string = '') {
+    document.querySelector('.edit')?.replaceChildren(value);
+  }
+
+  moveRight() {
+    var index = Number(document.querySelector('.edit')?.getAttributeNode('pos'));
+
+    if (index < 4) {
+      document.querySelector('.edit')?.parentElement?.children.item(index + 1)?.classList.add('edit');
+      document.querySelector('.edit')?.classList.remove('edit');
+    }
+  }
+
+  moveLeft() {
+    var index = Number(document.querySelector('.edit')?.getAttributeNode('pos'));
+
+    if (index > 0) {
+      document.querySelector('.edit')?.parentElement?.children.item(index - 1)?.classList.add('edit');
+      document.querySelectorAll('.edit')[1]?.classList.remove('edit');
+    }
+  }
 }
